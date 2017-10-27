28dbaaa6237ffc7afd5a169d7818638f2140050c
jvo <jvo@ccmbenchmark.com>
2017-10-27 17:40:11 +0200

    fix autoload

--- autoload.php
+++ autoload.php
@@ -3,6 +3,7 @@
 spl_autoload_register(function ($class) {
 
     $filename = str_replace('\\' , '/', $class);
+    $filename = str_replace('CCMBenchmark', 'src', $filename);
 
     require_once $filename . '.php';
 });
