d69bd5398cf69ea4f2c295468ebc2f306f0db1e4
jvo <jvo@ccmbenchmark.com>
2017-10-27 17:40:20 +0200

    add conf file for testing

---
+++ conf.php
@@ -0,0 +1,47 @@
+<?php
+
+$classNameFormatter = function ($tableName) {
+    return (string) str_replace('_', '', ucwords($tableName, '_'));
+};
+
+return [
+    /**
+     * Database
+     */
+    'type' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => '3302',
+    'databaseName' => 'auth_ccm_net',
+    'charset' => 'utf8',
+    'username' => 'mysql',
+    'password' => 'p455w0rd',
+
+    /**
+     * Return true if the specified table should be excluded.
+     */
+    'excludedTablesFilter' => function ($tableName) {
+        if ($tableName === 'DATABASECHANGELOG' || $tableName === 'DATABASECHANGELOGLOCK') {
+            return true;
+        }
+
+        return false;
+    },
+    /**
+     * Format name of generated entity from table's name.
+     * ucfirst by default. Overrode if you define 'entityNameFormatter'
+     */
+    'entityNameFormatter'     => $classNameFormatter,
+    'repositoryNameFormatter' => $classNameFormatter,
+
+    /**
+     * Where files will be written
+     */
+    'entitiesDirectory'       => '/var/www/POCTingGenerate/AppBundle/Domain/Entity',
+    'repositoriesDirectory'   => '/var/www/POCTingGenerate/AppBundle/Infrastructure/Repository',
+
+    /**
+     * Namespaces
+     */
+    'entityNamespace'         => 'AppBundle\Domain\Entity',
+    'repositoryNamespace'     => 'AppBundle\Infrastructure\Repository'
+];
