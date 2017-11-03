054401adb885ce36c8127ef1ceae97054f9724d2
jvo <jvo@ccmbenchmark.com>
2017-11-03 16:02:15 +0100

    maj commande GenerateEntity to generate source code and writing file

--- GenerateEntity.php
+++ GenerateEntity.php
@@ -1,13 +1,19 @@
 <?php
 
+namespace CCMBenchmark\TingGenerator;
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/autoload.php';
 
+use CCMBenchmark\TingGenerator\Entity\Generator;
+use CCMBenchmark\TingGenerator\Infrastructure\StringFormatter;
 use CCMBenchmark\TingGenerator\Log\Logger;
 use CCMBenchmark\TingGenerator\Configuration\ConfigurationLoader;
 use CCMBenchmark\TingGenerator\Database\MySQL\TableAnalyzer;
 use CCMBenchmark\TingGenerator\Configuration\Configuration;
 use CCMBenchmark\TingGenerator\Database\MySQL\TypeMapping;
+use Zend\Code\Generator\ClassGenerator;
+use Zend\Code\Generator\FileGenerator;
 
 class GenerateEntity
 {
@@ -15,6 +21,26 @@ class GenerateEntity
      * @var Logger
      */
     private $logger;
+
+    /**
+     * @var Generator
+     */
+    private $entityGenerator;
+
+    /**
+     * @var mixed|null
+     */
+    private $entityNameFormatter;
+
+    /**
+     * @var Configuration
+     */
+    private $configuration;
+
+    /**
+     * @var FileGenerator
+     */
+    private $fileGenerator;
 
     /**
      * GenerateEntity constructor.
@@ -25,18 +51,31 @@ class GenerateEntity
         $this->logger = $logger;
     }
 
+    /**
+     * @throws \Zend\Code\Generator\Exception\InvalidArgumentException
+     * @throws \Zend\Code\Generator\Exception\RuntimeException
+     */
     public function execute()
     {
-        $configuration = $this->getConfiguration(__DIR__ . '/conf.php');
-        if ($configuration === null) {
+        $this->configuration = $this->getConfiguration(__DIR__ . '/conf.php');
+        if ($this->configuration === null) {
             $this->logger->error('Configuration file not found');
             exit;
         }
 
-        $tablesData = $this->getTablesData($configuration);
+        $tablesData = $this->getTablesData();
         if ($tablesData === null) {
             $this->logger->error('Unable to read data source and retrieve table data');
             exit;
+        }
+
+        $this->fileGenerator = new FileGenerator();
+        $this->entityGenerator = new Generator(new ClassGenerator(), new Logger(), new StringFormatter());
+        $this->entityNameFormatter = $this->configuration->getEntityNameFormatter();
+
+        foreach ($tablesData as $tableName => $tableData) {
+            $this->logger->info('----------------------------------------------');
+            $this->generateEntity($tableName, $this->configuration->getEntityNamespace(), $tableData);
         }
     }
 
@@ -52,23 +91,96 @@ class GenerateEntity
     }
 
     /**
-     * @param Configuration $configuration
-     *
      * @return array
      */
-    private function getTablesData(Configuration $configuration)
+    private function getTablesData()
     {
-        $tableAnalyzer = new TableAnalyzer(new TypeMapping($this->logger), $this->logger);
+        $tableAnalyzer = new TableAnalyzer(new TypeMapping(), $this->logger);
         return $tableAnalyzer
             ->connect(
-                $configuration->getUsername(),
-                $configuration->getPassword(),
-                $configuration->getDatabaseName(),
-                $configuration->getHost(),
-                $configuration->getPort(),
-                $configuration->getCharset()
+                $this->configuration->getUsername(),
+                $this->configuration->getPassword(),
+                $this->configuration->getDatabaseName(),
+                $this->configuration->getHost(),
+                $this->configuration->getPort(),
+                $this->configuration->getCharset()
             )
-            ->getTablesData($configuration->getExcludedTablesFilter());
+            ->getTablesData($this->configuration->getExcludedTablesFilter());
+    }
+
+    /**
+     * Generate source code and write entity.
+     *
+     * @param string $entityName
+     * @param string $entityNamespace
+     * @param array $tableData
+     *
+     * @throws \Zend\Code\Generator\Exception\InvalidArgumentException
+     * @throws \Zend\Code\Generator\Exception\RuntimeException
+     *
+     * @return bool Return true on success, false on failure.
+     */
+    private function generateEntity($entityName, $entityNamespace, array $tableData)
+    {
+        $entityNameFormatted = $entityName;
+        if ($this->entityNameFormatter !== null) {
+            $formatter = $this->entityNameFormatter;
+            $entityNameFormatted = $formatter($entityName);
+        }
+
+        $this->logger->info('Generate entity: ' . $entityNameFormatted);
+
+        $classGenerator = $this
+            ->entityGenerator
+                ->generateEntityCode($entityNameFormatted, $entityNamespace, $tableData)
+                ->getClassGenerator();
+
+        return $this->writeEntity($entityNameFormatted, $classGenerator);
+    }
+
+    /**
+     * @param string $entityName
+     * @param ClassGenerator $classGenerator
+     *
+     * @throws \Zend\Code\Generator\Exception\InvalidArgumentException
+     * @throws \Zend\Code\Generator\Exception\RuntimeException
+     *
+     * @return bool Return true on success, false on failure.
+     */
+    private function writeEntity($entityName, $classGenerator)
+    {
+        $this->logger->info('Writing...');
+
+        $entityDirectory = $this->configuration->getEntityDirectory();
+        if ($entityDirectory === '') {
+            $this
+                ->logger
+                    ->error('You must specify a repository for generated entities. See entitiesDirectory in conf.');
+            return false;
+        }
+
+        $this->logger->info('... in directory: ' . $entityDirectory);
+
+        if (is_dir($entityDirectory) === false) {
+            $this->logger->info('Creating directory: ' . $entityDirectory);
+            if (mkdir($entityDirectory, 0777, true) === false) {
+                $this->logger->error('Unable to write directory: ' . $entityDirectory);
+                return false;
+            }
+        }
+
+        $fileGenerator = new FileGenerator();
+        $fileGenerator->setClass($classGenerator);
+
+        $filename = $entityDirectory . '/' . $entityName . '.php';
+        if (file_put_contents($filename, $fileGenerator->generate()) === false) {
+            $this->logger->error('Error when writing "' . $filename . '"');
+            return false;
+        }
+
+        $this->logger->info('Entity written in: ' . $filename);
+
+        return true;
     }
 }
 
