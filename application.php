1eee1c24fc3281bcd644fcc7c4938004428531e5
jvo <jvo@ccmbenchmark.com>
2017-11-17 16:30:26 +0100

    clean

--- application.php
+++ application.php
@@ -4,9 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/autoload.php';
 
 use Symfony\Component\Console\Application;
+use CCMBenchmark\TingGenerator\Command\TingGenerateCommand;
 
 $application = new Application();
-
-$application->add(new \CCMBenchmark\TingGenerator\Command\TingGenerateCommand());
-
+$application->add(new TingGenerateCommand());
 $application->run();
